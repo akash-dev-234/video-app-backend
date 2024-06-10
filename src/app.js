ee8e917b5a800c84bd4f235b524719dff7697526
northenlights <akash221002@gmail.com>
2024-06-10 23:36:51 +0530

    cors and cookle parser config

---
+++ src/app.js
@@ -0,0 +1,27 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+app.use(
+  express.json({
+    limit: "16kb",
+  })
+); //using this for our app to accept json data
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+); //using this for our app to accept data from url
+app.use(express.static("public")); // to store static assets that can accessible to anyone
+app.use(cookieParser());
+
+export { app };
--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 //connect db using mongoose
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port: ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Mongo db connection failed", err);
+  });
 
 //this is immediately invoked function common syntax used for connecting db(old way)
 
