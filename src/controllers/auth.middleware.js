3454057305e018725c9ad89ee5aed706e2e62cfd
northenligths <akash221002@gmail.com>
2024-06-26 20:58:49 +0530

    bug fixes for token and user logout

--- src/controllers/auth.middleware.js
+++ src/controllers/auth.middleware.js
@@ -1,5 +1,6 @@
 import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { User } from "../models/user.model.js";
 import jwt from "jsonwebtoken";
 
 export const verifyJWT = asyncHandler(async (req, res, next) => {
@@ -21,5 +22,7 @@ export const verifyJWT = asyncHandler(async (req, res, next) => {
     }
     req.user = user;
     next();
-  } catch (err) {}
+  } catch (err) {
+    console.log(err);
+  }
 });
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -137,7 +137,8 @@ const logoutUser = asyncHandler(async (req, res) => {
     httpOnly: true,
     secure: true,
   };
-  return res.status
+  return res
+    .status(200)
     .clearCookie("accessToken", options)
     .clearCookie("refreshToken", options)
     .json(new ApiResponse(200, {}, "User logged Out"));
