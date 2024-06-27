304c62696d1a1b9c378cfbad9ff68ef69983c8cf
northenligths <akash221002@gmail.com>
2024-06-27 18:53:37 +0530

    added routes for update user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -198,7 +198,7 @@ const changeCurrentPassword = asyncHandler(async (req, res) => {
 const getCurrentUser = asyncHandler(async (req, res) => {
   return res
     .status(200)
-    .json(200, req.user, "current user fetched successfully");
+    .json(new ApiResponse(200, req.user, "User fetched successfully"));
 });
 
 const updateAccountDetails = asyncHandler(async (req, res) => {
@@ -208,13 +208,18 @@ const updateAccountDetails = asyncHandler(async (req, res) => {
     throw new ApiError(400, "All fields are required");
   }
 
-  const user = User.findByIdAndUpdate(
+  const user = await User.findByIdAndUpdate(
     req.user?._id,
-    { $set: { fullName, email } },
+    {
+      $set: {
+        fullName,
+        email: email,
+      },
+    },
     { new: true }
   ).select("-password");
 
-  res
+  return res
     .status(200)
     .json(new ApiResponse(200, user, "Account details updated successfully"));
 });
